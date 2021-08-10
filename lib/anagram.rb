36b9f92218edf5f3cb3f19e759e2996dcf363872
Lex Nelson <lexinelson597@gmail.com>
2021-08-10 11:16:24 -0400

    commit tree 8347b127e3bacbdc80e8ab41b09aecfdf55f10a0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word= word
+    end
+    def match(arr)
+        same = []
+        new = arr.filter do |w|
+            w.chars.sort == word.chars.sort
+        end
+        new
+    end
+end
